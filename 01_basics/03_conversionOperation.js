0b0f1261bd7b14bd147e4d41216c89fdaf157b72
shubham verma <101853403+Shubham25122001@users.noreply.github.com>
2023-11-04 14:32:41 +0530

    03_conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,13 +2,13 @@
 // let score = null;
 let score = true;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -18,9 +18,60 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber, typeof stringNumber);
+// console.log(stringNumber, typeof stringNumber);
+
+
+
+
+
+// ************************ Operations **************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 3);
+// console.log(2 - 3);
+// console.log(2 * 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+// console.log(2 ** 3);
+
+
+
+let str1 = "shubham";
+let str2 = " Verma";
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); //32
+
+
+
+// console.log(true); // true
+// console.log(+true); // 1
+
+// console.log(+""); // 0
+
+
+
+
+
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
